928c57d589710d0ce94ca5493aabf4e5c0dcd952
Julian <julian@wagnerjw.dev>
2022-05-15 20:57:16 -0700

    initial commit

---
+++ main.py
@@ -0,0 +1,31 @@
+import json
+from recipe_scrapers import scrape_me
+
+recipeURL = input("Enter Recipe URL Here:... ")
+
+scraper = scrape_me(recipeURL)
+
+title = scraper.title()
+total_time = scraper.total_time()
+ingredients = scraper.ingredients()
+instructions = scraper.instructions()
+yields = scraper.yields()
+image = scraper.image()
+host = scraper.host()
+nutrients = scraper.nutrients()
+
+recipeComponents ={
+  'title' : scraper.title(),
+  'ingredients' : scraper.ingredients(),
+  'instructions' : instructions.replace("\n", " "),
+  'yield' : yields,
+  'image' : image,
+  'host' : host,
+  'nutrients' : nutrients,
+}
+
+json_object = json.dumps(recipeComponents, indent = 4)
+
+print(json_object)
+
+
